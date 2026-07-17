fd1a2f29da7d4e1b0484a2e0b9b1572eb28d7375
ZidanArdi <panjihadi6@gmail.com>
2026-07-17 13:47:55 +0700

    pushh

--- config/cors.go
+++ config/cors.go
@@ -3,7 +3,7 @@ package config
 var allowedOrigins = []string{
 	"http://localhost:5173",
 	"http://localhost:5174",
-	"https://my-fe-ten.vercel.app", // This will be replaced with the actual Vercel URL
+	"https://sisteminformasisekolahbackend-production.up.railway.app",
 }
 
 func GetAllowedOrigins() []string {
