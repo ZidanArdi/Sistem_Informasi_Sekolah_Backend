76658ba8543109ef6f9b96d6de30993e74063df3
ZidanArdi <panjihadi6@gmail.com>
2026-07-17 15:30:05 +0700

    pushhhhhh

--- config/cors.go
+++ config/cors.go
@@ -4,6 +4,7 @@ var allowedOrigins = []string{
 	"http://localhost:5173",
 	"http://localhost:5174",
 	"https://sisteminformasisekolahbackend-production.up.railway.app",
+	"https://sistem-informasi-sekolah-frontend.vercel.app",
 }
 
 func GetAllowedOrigins() []string {
